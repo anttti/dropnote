f7078151201ea87d8a14cf9f556f0798457ee428
Antti Mattila <antti.mattila@beehealthy.com>
2025-11-27 08:38:03 +0200

    Initial Commit

---
+++ Dropnote/DropnoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropnoteApp.swift
+//  Dropnote
+//
+//  Created by Antti Mattila on 27.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct DropnoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DropnoteTests/DropnoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  DropnoteTests.swift
+//  DropnoteTests
+//
+//  Created by Antti Mattila on 27.11.2025.
+//
+
+import Testing
+@testable import Dropnote
+
+struct DropnoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
